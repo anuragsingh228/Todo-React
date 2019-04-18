19c535648f13e5f0622cd2ac77b59708037622a6
Anurag Singh <anurags228@gmail.com>
2019-04-18 14:07:50 +0530

    todo component created

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Components {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default TodoItem
---
+++ src/components/Todos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        return this.props.todos.map((todo) => (
+            <h3> {todo.tittle} </h3>
+        ));
+    }
+}
+
+export default Todos;
